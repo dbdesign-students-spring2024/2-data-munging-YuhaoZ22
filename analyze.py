e151a3b1204fd7a785b92302bd1d12354d7f8bad
YuhaoZ22 <yz7623@nyu.edu>
2024-02-13 01:27:21 -0500

    Assignment 2 Yuhao & Anyu

--- analyze.py
+++ analyze.py
@@ -1,1 +1,28 @@
-# Place code below to do the analysis part of the assignment.
+def averages():
+     
+    with open("/Users/zyh/Desktop/2-data-munging-YuhaoZ22/data/clean_data.csv", "r") as file:
+        next(file)  
+        
+        # Initialize variables
+        data = []
+        for line in file:
+            parts = line.strip().split(',')
+            data.append(float(parts[1]))  # Assuming anomalies are in 2nd column
+
+    # Process data
+    start = 1880
+    sum_temp, count = 0, 0
+
+    for i, temp in enumerate(data):
+        sum_temp += temp
+        count += 1
+
+        if (i + 1) % 10 == 0 or i + 1 == len(data):
+            avg_temp = sum_temp / count
+            print(f"{start} to {start + 9}: {round(avg_temp, 2)}")
+            
+            # Update for next decade
+            start += 10
+            sum_temp, count = 0, 0
+
+averages()
